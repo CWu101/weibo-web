fff567ca94d1fd8bc923d12f06b2ffbad3435a80
charleswu <charleswu101@outlook.com>
2024-09-19 14:48:07 +0800

    add wgsi

---
+++ wgsi.py
@@ -0,0 +1,12 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
+import sys
+
+
+# import flask app but need to call it "application" for WSGI to work
+from app import app as application  # noqa
